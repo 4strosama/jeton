02bf0024c682013d5671a8bcaf80cd86d176344f
4strosama <199502660+4strosama@users.noreply.github.com>
2025-02-19 01:16:13 +0800

    feat: directory structure

---
+++ src/tokenizers/builder.rs
@@ -0,0 +1,27 @@
+use crate::decoders::Decoder;
+use crate::models::Model;
+use crate::normalizers::Normalizer;
+
+use super::post_processor::PostProcessor;
+use super::pre_tokenizer::PreTokenizer;
+
+pub struct TokenizerBuilder<M, N, PT, PP, D> {
+    model: Option<M>,
+    normalizer: Option<N>,
+    pre_tokenizer: Option<PT>,
+    post_processor: Option<PP>,
+    decoder: Option<D>
+}
+
+impl<M, N, PT, PP, D> Default for TokenizerBuilder<M, N, PT, PP, D>
+where
+    M: Model,
+    N: Normalizer, 
+    PT: PreTokenizer,
+    PP: PostProcessor,
+    D: Decoder
+{
+    fn default() -> Self {
+        Self {}
+    }
+}
---
+++ src/tokenizers/mod.rs
@@ -0,0 +1,3 @@
+pub mod builder;
+pub mod post_processor;
+pub mod pre_tokenizer;
