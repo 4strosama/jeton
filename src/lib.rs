655742ebc36aec943632931a46275c631f7e202f
4strosama <199502660+4strosama@users.noreply.github.com>
2025-02-19 16:08:02 +0800

    feat: re-export

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,14 @@ pub mod models;
 pub mod normalizers;
 pub mod tokenizers;
 pub mod trainer;
+pub mod utils;
+
+pub use tokenizers::builder::*;
+pub use tokenizers::extensions::*;
+pub use tokenizers::implementation::*;
+pub use tokenizers::post_processor::*;
+pub use tokenizers::pre_tokenizer::*;
+
+pub mod prelude {
+    pub struct TokenizerBuilder;
+}
