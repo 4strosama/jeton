e7e173a01c489f10c56e390e05aac020b53d3983
4strosama <199502660+4strosama@users.noreply.github.com>
2025-02-19 16:19:00 +0800

    feat: encoding

---
+++ src/tokenizers/encoding.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::ops::Range;
+
+use serde::{Deserialize, Serialize};
+
+use super::Offset;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
+pub struct Encoding {
+    id: Vec<u32>,
+    id_type: Vec<u32>,
+    tokens: Vec<String>,
+    words: Vec<Option<u32>>,
+    offsets: Vec<Offset>,
+    special_tokens_mask: Vec<u32>,
+    attention_mask: Vec<u32>,
+    overflowing: Vec<Encoding>,
+    sequence_ranges: HashMap<usize, Range<usize>>,
+}
+
+impl Encoding {
+    pub fn new(
+        id: Vec<u32>,
+        id_type: Vec<u32>,
+        tokens: Vec<String>,
+        words: Vec<Option<u32>>,
+        offsets: Vec<Offset>,
+        special_tokens_mask: Vec<u32>,
+        attention_mask: Vec<u32>,
+        overflowing: Vec<Encoding>,
+        sequence_ranges: HashMap<usize, Range<usize>>
+    ) -> Self {
+        Self {
+            id,
+            id_type,
+            tokens,
+            words,
+            offsets,
+            special_tokens_mask,
+            attention_mask,
+            overflowing,
+            sequence_ranges
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            id: Vec::with_capacity(capacity),
+            id_type: Vec::with_capacity(capacity),
+            tokens: Vec::with_capacity(capacity),
+            words: Vec::with_capacity(capacity),
+            offsets: Vec::with_capacity(capacity),
+            special_tokens_mask: Vec::with_capacity(capacity),
+            attention_mask: Vec::with_capacity(capacity),
+            overflowing: Vec::new(),
+            sequence_ranges: HashMap::new()
+        }
+    }
+}
--- src/tokenizers/mod.rs
+++ src/tokenizers/mod.rs
@@ -1,5 +1,10 @@
 pub mod builder;
+pub mod encoding;
 pub mod extensions;
 pub mod implementation;
 pub mod post_processor;
 pub mod pre_tokenizer;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T> = std::result::Result<T, Error>;
+pub type Offset = (usize, usize);
